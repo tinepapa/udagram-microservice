b10081b44fffe0f1a33e505f875d4dd7f8a9c3df
tinepapa <tinembaye@hotmail.com>
2022-08-31 17:19:32 +0000

    Update k8s deployment files

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5a87fbc6960442f59755a2f4b50575d-1675507793.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
